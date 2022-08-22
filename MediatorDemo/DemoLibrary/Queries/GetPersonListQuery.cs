482901fa244590841bc21f5720875dbb0297dc2f
shahedmahmoudi <33050673+shahedmahmoudi@users.noreply.github.com>
2022-08-23 01:02:16 +0200

    GetPersonListQuery

---
+++ MediatorDemo/DemoLibrary/Queries/GetPersonListQuery.cs
@@ -0,0 +1,13 @@
+﻿using DemoLibrary.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoLibrary.Queries
+{
+    public class GetPersonListQuery : IRequest<IEnumerable<PesonModel>>
+    {
+
+    }   
+}
