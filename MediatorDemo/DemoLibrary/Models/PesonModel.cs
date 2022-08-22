2eaa1fdb0935556d6df49ca93e1d407eaff1fd64
shahedmahmoudi <33050673+shahedmahmoudi@users.noreply.github.com>
2022-08-23 00:17:43 +0200

    create model and data acsess Person

--- MediatorDemo/DemoLibrary/Models/PesonModel.cs
+++ MediatorDemo/DemoLibrary/Models/PesonModel.cs
@@ -6,5 +6,8 @@ namespace DemoLibrary.Models
 {
     class PesonModel
     {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
     }
 }
