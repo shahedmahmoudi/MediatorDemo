08a3fd10f18d8fff7b22afcba12136dfa11a6802
shahedmahmoudi <33050673+shahedmahmoudi@users.noreply.github.com>
2022-08-23 13:30:14 +0200

    Get by id Handler

---
+++ MediatorDemo/DemoLibrary/Handlers/GetPersonByIdHandler.cs
@@ -0,0 +1,27 @@
+﻿using DemoLibrary.Models;
+using DemoLibrary.Queries;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.Handlers
+{
+    public class GetPersonByIdHandler:IRequestHandler<GetPersonByIdQuery,PesonModel>
+    {
+        private readonly IMediator _mediator;
+        public GetPersonByIdHandler(IMediator mediator)
+        {
+
+        }
+        public async Task<PesonModel> Handle(GetPersonByIdQuery request,CancellationToken canclelationToken)
+        {
+            var result = await _mediator.Send(new GetPersonListQuery());
+            var output = result.FirstOrDefault(x => x.Id == request.Id);
+            return output;
+        }
+    }
+}
